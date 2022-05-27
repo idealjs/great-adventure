92dabdb2580b27e2c0ba3a75961fac3db25aac9e
cqh963852 <cqh963852@hotmail.com>
2022-05-27 23:31:52 +0800

    update: update style

--- packages/app/components/Map/Routes.tsx
+++ packages/app/components/Map/Routes.tsx
@@ -21,30 +21,42 @@ const Route = (props: IProps) => {
   const [cachedPlace, setCachedPlace] = useState(() => place);
 
   return (
-    <div className={clsx("border-l-4 ml-2 pl-2", { hidden, [placeId]: true })}>
-      <div className="flex items-center	justify-between my-2">
-        <button
-          onClick={() => {
-            setChecked((c) => !c);
-            setCachedPlace(place);
-          }}
-          className="w-6"
-        >
-          {checked ? (
-            <FolderOpenIcon className="text-gray-500" />
-          ) : (
-            <FolderIcon className="text-gray-500" />
-          )}
-        </button>
+    <div>
+      <div className={clsx({ hidden })}>
+        <div className="flex items-center	justify-between my-2">
+          <button
+            onClick={() => {
+              setChecked((c) => !c);
+              setCachedPlace(place);
+            }}
+            className="w-6"
+          >
+            {checked ? (
+              <FolderOpenIcon className="text-gray-500" />
+            ) : (
+              <FolderIcon className="text-gray-500" />
+            )}
+          </button>
 
-        <label className="mx-1">{place?.name}</label>
-        <button onClick={() => mutate()} className="w-6">
-          <PaperAirplaneIcon className="text-gray-500	rotate-90" />
-        </button>
+          <label className="mx-1">{place?.name}</label>
+          <button onClick={() => mutate()} className="w-6">
+            <PaperAirplaneIcon className="text-gray-500	rotate-90" />
+          </button>
+        </div>
       </div>
-
-      {(place ?? cachedPlace)?.travelRoutes?.map((r) => {
-        return <Route key={r.to} placeId={r.to} hidden={!checked} />;
+      {(place ?? cachedPlace)?.travelRoutes?.map((r, index, array) => {
+        return (
+          <div key={r.to} className="flex">
+            <div
+              className={clsx("border-l-4 border-b-4 ml-2 pl-2", {
+                hidden: !checked,
+                "h-3": index === array.length - 1,
+                "h-5": index !== array.length - 1,
+              })}
+            ></div>
+            <Route key={r.to} placeId={r.to} hidden={!checked} />
+          </div>
+        );
       })}
     </div>
   );
